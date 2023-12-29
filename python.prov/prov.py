14964ad0864c4c7cb97d72a677722bba6d7725d3
ardit2701 <ardit.raseni@gmail.com>
2023-12-29 16:12:53 +0100

    Committing changes in fundamentals submodule in the main repository

---
+++ python.prov/prov.py
@@ -0,0 +1,21 @@
+def sayMyName(name='Haisenberg'):
+    pass
+
+name = sayMyName('isuf')
+
+person = {
+    'firstname':'Ardit',
+    'lastname':'Raseni',
+    'age':'21'
+}
+print(person['firstname'],person['lastname'])
+shel: 5+5
+print(1 + str(2))
+print(str(1) + str(2))
+print(int(1) + 2)
+print(1+2)
+name = Charles
+'Hello' + 'World'
+Hello + World
+'Hello' . 'World'
+All of the above
